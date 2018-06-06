3b9ea45c9ff369b563ba26bd4ec21d3d8216f167
christianbondesen <cbb@it-minds.dk>
2018-06-06 10:44:34 +0200

    refactored inputs and selection fields

--- src/components/details/general-information/GeneralInformationForm.vue
+++ src/components/details/general-information/GeneralInformationForm.vue
@@ -1,28 +1,48 @@
 <template>
     <div class="container">
         <div class="section-col kle">
-            <div class="labels">
-                <div>KLE-nr:</div>
-                <div>Lov og paragraf:</div>
-            </div>
-            <div class="fields">
-                <KleNumberField />
-                <ParagrafField />
-            </div>
+          <div class="labels">
+              <div>KLE-nr:</div>
+              <div>Lov og paragraf:</div>
+          </div>
+          <div class="fields">
+              <SelectionField :value="getKleNumber" @change="setKleNumber" :fields="kleNumber" />
+              <InputField :value="getParagraf" @input="setParagraf" />
+          </div>
         </div>
         <div class="section-col">
-            <FieldSelection />
-            <DepartmentField />
-            <VisibilitySelection />
+          <div class="labels">
+            <div>Afdeling:</div>
+            <div>Fagområde:</div>
+            <div>Synlighed:</div>
+          </div>
+          <div class="fields">
+            <InputField :value="getDepartment" @input="setDepartment"/>
+            <SelectionField :value="getField" @change="setField" :fields="fields" />
+            <SelectionField :value="getVisibility" @change="setVisibility" :fields="visibilityItems"/>
+          </div>
         </div>
         <div class="section-col">
-            <ContactPersonField />
-            <EmailField />
-            <ProcesTimeField />
+          <div class="labels">
+            <div>Kontaktperson:</div>
+            <div>Mail:</div>
+            <div>Procestid:</div>
+          </div>
+          <div class="fields">
+            <InputField :value="getContactPerson" @input="setContactPerson"/>
+            <InputField :value="getEmail" @input="setEmail"/>
+            <InputField :value="getProcesTime" @input="setProcesTime"/>
+          </div>
         </div>
         <div class="section-col">
-            <SupplierField />
-            <ProjectManagerField />
+          <div class="labels">
+            <div>Leverandør:</div>
+            <div>Projektleder:</div>
+          </div>
+          <div class="fields">
+            <InputField :value="getSupplier" @input="setSupplier"/>
+            <InputField :value="getProjectMananger" @input="setProjectMananger"/>
+          </div> 
         </div>
         <!-- <ResumeTextArea /> -->
     </div>
@@ -30,60 +50,113 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { Action } from 'vuex-class';
 
-import KleNumberField from '@/components/details/general-information/inputs/KleNumberField.vue';
-import ParagrafField from '@/components/details/general-information/inputs/ParagrafField.vue';
-import FieldSelection from '@/components/details/general-information/inputs/FieldSelection.vue';
-import DepartmentField from '@/components/details/general-information/inputs/DepartmentField.vue';
-import VisibilitySelection from '@/components/details/general-information/inputs/VisibilitySelection.vue';
-import ContactPersonField from '@/components/details/general-information/inputs/ContactPersonField.vue';
-import EmailField from '@/components/details/general-information/inputs/EmailField.vue';
-import ProcesTimeField from '@/components/details/general-information/inputs/ProcesTimeField.vue';
-import ProjectManagerField from '@/components/details/general-information/inputs/ProjectManagerField.vue';
-import SupplierField from '@/components/details/general-information/inputs/SupplierField.vue';
-import ResumeTextArea from '@/components/details/general-information/inputs/ResumeTextArea.vue';
+import InputField from '@/components/details/general-information/inputs/InputField.vue';
+import SelectionField from '@/components/details/general-information/inputs/SelectionField.vue';
+
+const namespace = 'generalInformation';
 
 @Component({
-    components: {
-        KleNumberField,
-        ParagrafField,
-        FieldSelection,
-        DepartmentField,
-        VisibilitySelection,
-        ContactPersonField,
-        EmailField,
-        ProcesTimeField,
-        ProjectManagerField,
-        SupplierField,
-        ResumeTextArea
-    }
+  components: {
+    InputField,
+    SelectionField,
+  }
 })
 export default class GeneralInformationForm extends Vue {
+  @Action('setKleNumber', { namespace })
+  setKleNumber: any;
+  @Action('setParagraf', { namespace })
+  setParagraf: any;
+  @Action('setDepartment', { namespace })
+  setDepartment: any;
+  @Action('setContactPerson', { namespace })
+  setContactPerson: any;
+  @Action('setEmail', { namespace })
+  setEmail: any;
+  @Action('setProcesTime', { namespace })
+  setProcesTime: any;
+  @Action('setSupplier', { namespace })
+  setSupplier: any;
+  @Action('setProjectMananger', { namespace })
+  setProjectMananger: any;
+  @Action('setField', { namespace })
+  setField: any;
+  @Action('setVisibility', { namespace })
+  setVisibility: any;
 
+  fields = [
+      'Teknik',
+      'Diverse',
+      'ETC'
+  ];
+
+  visibilityItems = [
+      'Privat',
+      'Tværkommunalt',
+      'Kommunalt'
+  ];
+
+  kleNumber = [
+    '1234',
+    '134324',
+    '54353'
+  ];
+
+  get getKleNumber() {
+    return this.$store.state.kleNumber;
+  }
+  get getParagraf() {
+    return this.$store.state.paragraf;
+  }
+  get getDepartment() {
+    return this.$store.state.department;
+  }
+  get getContactPerson() {
+    return this.$store.state.contactPerson;
+  }
+  get getEmail() {
+    return this.$store.state.email;
+  }
+  get getProcesTime() {
+    return this.$store.state.procesTime;
+  }
+  get getSupplier() {
+    return this.$store.state.supplier;
+  }
+  get getProjectMananger() {
+    return this.$store.state.projectManager;
+  }
+  get getField() {
+    return this.$store.state.field;
+  }
+  get getVisibility() {
+    return this.$store.state.visibility;
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-    .container {
-        display: flex;
-        flex-direction: row;
-        background-color: gainsboro;
+.container {
+  display: flex;
+  flex-direction: row;
+  background-color: gainsboro;
+}
+.section-col {
+  flex: 0 0 25%;
+  display: flex;
+
+  .labels {
+    width: 50%;
+
+    > div {
+      line-height: 32px;
     }
-    .section-col {
-        flex: 0 0 25%;
-        display: flex;
+  }
 
-        .labels {
-            width: 50%;
-
-            > div {
-                line-height: 32px;
-            }
-        }
-
-        .labels > div,
-        .fields > * {
-            margin-bottom: 1rem;
-        }
-    }
+  .labels > div,
+  .fields > * {
+    margin-bottom: 1rem;
+  }
+}
 </style>
