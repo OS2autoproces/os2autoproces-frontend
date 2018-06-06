1527f3d6133d2cc865245bf2ec9341fbedcd028e
christianbondesen <cbb@it-minds.dk>
2018-06-06 13:33:34 +0200

    constriants

--- src/store/modules/details/general-information/getters.ts
+++ src/store/modules/details/general-information/getters.ts
@@ -3,82 +3,64 @@ import { GeneralInformationState } from '@/store/modules/details/general-informa
 import { GetterTree } from 'vuex';
 import * as validateJs from 'validate.js';
 
-export const getters: GetterTree<GeneralInformationState, RootState> = {
-  isKleNumberValid(state: GeneralInformationState): boolean {
-    return isValid(state.kleNumber, {
-      numericality: true
-    });
-  },
-  isParagrafValid(state: GeneralInformationState): boolean {
-    return isValid(state.paragraf, {
-      length: {
-        minimum: 1
-      }
-    });
-  },
-  isFieldValid(state: GeneralInformationState): boolean {
-    return isValid(state.field, {
-      length: {
-        minimum: 1
-      }
-    });
-  },
-  isDepartmentValid(state: GeneralInformationState): boolean {
-    return isValid(state.department, {
-      length: {
-        minimum: 1
-      }
-    });
-  },
-  isVisibilityValid(state: GeneralInformationState): boolean {
-    return isValid(state.visibility, {
-      length: {
-        minimum: 1
-      }
-    });
-  },
-  isContactPersonValid(state: GeneralInformationState): boolean {
-    return isValid(state.contactPerson, {
-      length: {
-        minimum: 1
-      }
-    });
-  },
-  isEmailValid(state: GeneralInformationState): boolean {
-    return isValid(state.email, {
-      email: true
-    });
-  },
-  isProcesTimeValid(state: GeneralInformationState): boolean {
-    return isValid(state.procesTime, {
-      numericality: true
-    });
-  },
-  isProjectManagerValid(state: GeneralInformationState): boolean {
-    return isValid(state.projectManager, {
-      length: {
-        minimum: 1
-      }
-    });
-  },
-  isSupplierValid(state: GeneralInformationState): boolean {
-    return isValid(state.supplier, {
-      length: {
-        minimum: 2
-      },
-    });
-  },
-  isResumeValid(state: GeneralInformationState): boolean {
-    return isValid(state.resume, {
-      presence: true,
-      length: {
-        minimum: 10,
-        maximum: 140
-      }
-    });
+const isNonempty = {
+  length: {
+    minimum: 1
   }
 };
+
+const isNumeric = {
+  numericality: true,
+}
+
+const isEmail = {
+  email: true,
+}
+
+const isMinMax = (minimum: number, maximum: number) => ({
+  length: {
+    minimum,
+    maximum,
+  }
+});
 
 function isValid(value: any, constraints: any): boolean {
   return !validateJs({ value }, { value: constraints });
 }
+
+
+export const getters: GetterTree<GeneralInformationState, RootState> = {
+  isKleNumberValid(state: GeneralInformationState): boolean {
+    return isValid(state.kleNumber, isNumeric);
+  },
+  isParagrafValid(state: GeneralInformationState): boolean {
+    return isValid(state.paragraf, isNonempty);
+  },
+  isFieldValid(state: GeneralInformationState): boolean {
+    return isValid(state.field, isNonempty);
+  },
+  isDepartmentValid(state: GeneralInformationState): boolean {
+    return isValid(state.department, isNonempty);
+  },
+  isVisibilityValid(state: GeneralInformationState): boolean {
+    return isValid(state.visibility, isNonempty);
+  },
+  isContactPersonValid(state: GeneralInformationState): boolean {
+    return isValid(state.contactPerson, isNonempty);
+  },
+  isEmailValid(state: GeneralInformationState): boolean {
+    return isValid(state.email, isEmail);
+  },
+  isProcesTimeValid(state: GeneralInformationState): boolean {
+    return isValid(state.procesTime, isNumeric);
+  },
+  isProjectManagerValid(state: GeneralInformationState): boolean {
+    return isValid(state.projectManager, isNonempty);
+  },
+  isSupplierValid(state: GeneralInformationState): boolean {
+    return isValid(state.supplier, isNonempty);
+  },
+  isResumeValid(state: GeneralInformationState): boolean {
+    return isValid(state.resume, isMinMax(1, 140));
+  }
+};
