a32e15dc3fe03a6d98c5a5f9528faed3ee0c5fc7
christianbondesen <cbb@it-minds.dk>
2018-06-07 16:59:54 +0200

    deleted classes for circle and lines

--- src/components/icons/DeleteCrossIcon.vue
+++ src/components/icons/DeleteCrossIcon.vue
@@ -1,8 +1,8 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.56 18.56">
-        <circle class="cls-1" cx="9.28" cy="9.28" r="9.28"/>
-        <line class="cls-2" x1="5.6" y1="13.43" x2="12.99" y2="5.42"/>
-        <line class="cls-2" x1="13.36" y1="12.91" x2="5.09" y2="5.82"/>
+        <circle cx="9.28" cy="9.28" r="9.28"/>
+        <line x1="5.6" y1="13.43" x2="12.99" y2="5.42"/>
+        <line x1="13.36" y1="12.91" x2="5.09" y2="5.82"/>
     </svg>
 </template>
 
@@ -16,10 +16,11 @@ export default class DeleteCrossIcon extends Vue {}
 <style lang="scss" scoped>
 @import "@/styles/variables.scss";
 
-.cls-1 {
+circle {
     fill: $color-primary;
 }
-.cls-2 {
+
+line {
     fill: none;
     stroke: $color-background;
     stroke-miterlimit: 10;
