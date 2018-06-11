56dd337dc7795e987ac1cefa7184146537d39af5
christianbondesen <cbb@it-minds.dk>
2018-06-11 10:34:18 +0200

    routing for details form

---
+++ src/components/details/FormWrapper.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="form-border">
+        <GeneralInformationForm />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+import GeneralInformationForm from './general-information/GeneralInformationForm.vue';
+
+@Component({
+    components: {
+        GeneralInformationForm
+    }
+})
+export default class FormWrapper extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables.scss';
+    .form-border {
+        border-width: 1px;
+        border-style: solid;
+        margin: 10px;
+        border-color: $color-secondary;
+        padding: 2%;
+    }
+</style>
---
+++ src/views/ProcesForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <FormWrapper />
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+import FormWrapper from '@/components/details/FormWrapper.vue';
+
+@Component({
+    components: {
+        FormWrapper
+    }
+})
+export default class ProcesForm extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
