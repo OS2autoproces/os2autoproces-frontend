d2af9f85b372608c040ae160efca75aeded04d41
christianbondesen <cbb@it-minds.dk>
2018-06-11 15:15:41 +0200

    Description for issuepanel

--- src/components/details/issue-panel/IssuePanelForm.vue
+++ src/components/details/issue-panel/IssuePanelForm.vue
@@ -1,18 +1,34 @@
 <template>
     <div class="issue-panel-form">
-        <div :class="{'double-column': this.$store.state.details.issuePanel.description > 1600 }" class="issue-description">
-            {{this.$store.state.details.issuePanel.description}}
+        <h2>Beskrivelse</h2>
+        <TextArea v-if="!disabled" @change="updateIssuePanel({description: $event})" :disabled="disabled" :value="issuePanel.description" />
+        <div v-if="disabled" :class="{'double-column':issuePanel.description.length > 1600 }" class="issue-description">
+            {{issuePanel.description}}
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-import { namespace } from '@/store/modules/details/issue-panel/actions';
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import {
+  namespace,
+  issuePanelActionTypes
+} from '@/store/modules/details/issue-panel/actions';
+import TextArea from '@/components/common/inputs/TextArea.vue';
+import { Action } from 'vuex-class';
 
-@Component({})
+@Component({
+  components: {
+    TextArea
+  }
+})
 export default class GeneralInformationForm extends Vue {
+  @Action(issuePanelActionTypes.UPDATE_ISSUE_PANEL) updateIssuePanel: any;
+  @Prop() disabled!: boolean;
 
+  get issuePanel() {
+    return this.$store.state.details.issuePanel;
+  }
 }
 </script>
 
@@ -20,14 +36,18 @@ export default class GeneralInformationForm extends Vue {
 @import '@/styles/variables.scss';
 
 .issue-panel-form {
-    display: flex;
-
-    .issue-description {
-        width: 40%;
+  .issue-description {
+    width: 40%;
+    &.double-column {
+      width: 100%;
+      column-count: 2;
+      column-gap: 5rem;
     }
-    .double-column {
-        column-count: 2;
-        column-gap: 5rem;
-    }
+  }
+  h2 {
+    @include heading;
+    color: $color-secondary;
+    margin-bottom: $size-unit;
+  }
 }
 </style>
