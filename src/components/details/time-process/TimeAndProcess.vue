87ddcdb1e4e96a6cb3ad7cf10889c0b65bdac5fc
christianbondesen <cbb@it-minds.dk>
2018-06-13 13:37:39 +0200

    Time and process flex layout

--- src/components/details/time-process/TimeAndProcess.vue
+++ src/components/details/time-process/TimeAndProcess.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
-      sdfsdfsdf
+    <div class="time-and-process">
       <div class="comments-wrap">
-        <div>Kommentar vedr. tidsforbrug</div>
-        <InfoTooltip>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse unde neque eos, non et vel, aspernatur quod dolore perspiciatis dolorem minus rerum amet animi architecto mollitia alias! Debitis, eveniet sint!
-        </InfoTooltip>
+        <div>
+          Kommentar vedr. tidsforbrug
+          <InfoTooltip>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse unde neque eos, non et vel, aspernatur quod dolore perspiciatis dolorem minus rerum amet animi architecto mollitia alias! Debitis, eveniet sint!
+          </InfoTooltip>
+        </div>
         <TextArea :disabled="disabled" />
       </div>
     </div>
@@ -13,13 +14,13 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
-import GreyInputBox from '@/components/common/GreyInputBox.vue';
+// import GreyInputBox from '@/components/common/GreyInputBox.vue';
 import InfoTooltip from '@/components/common/InfoTooltip.vue';
 import TextArea from '@/components/common/inputs/TextArea.vue';
 
 @Component({
   components: {
-    GreyInputBox,
+    // GreyInputBox,
     InfoTooltip,
     TextArea
   }
@@ -32,8 +33,10 @@ export default class TimeAndProcess extends Vue {
 <style lang="scss" scoped>
 @import '@/styles/variables.scss';
 
-.comments-wrap {
-  width: 50%;
+.time-and-process {
+  display: flex;
+  .comments-wrap {
+    width: 50%;
+  }
 }
-
 </style>
