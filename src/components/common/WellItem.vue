a54acb4012f90afb585a2397a28968782884ec7d
christianbondesen <cbb@it-minds.dk>
2018-06-20 08:54:16 +0200

    Created well item to simply boiler plate for the well components. Formatting of text when no text is shown fixed

---
+++ src/components/common/WellItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="well-item-wrap">
+        <div class="well-item-label">{{label}}</div>
+        <slot class="well-input" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class WellItem extends Vue {
+@Prop() label!: string;
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/variables.scss';
+
+.well-item-wrap {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: $size-unit;
+
+    .well-item-label {
+        width: 70%;
+    }
+
+    .well-input {
+        width: 30%;
+    }
+}
+</style>
