5595f6640cfd8fd1f8b86df2893b29d24f6d9d02
christianbondesen <cbb@it-minds.dk>
2018-06-20 13:19:20 +0200

    some formatting + mixin for labels

--- src/components/details/general-information/AssociatedPersonsInput.vue
+++ src/components/details/general-information/AssociatedPersonsInput.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="associated">
-        <div class="associated-list" v-if="!disabled">
-            <div class="associated-label">Tilknyttede personer:</div>
-            <div class="associated-persons-list">
-                <div v-for="(person, index) in associatedPeople" :key="index">
-                    {{person}}
-                    <span @click="removePerson(person)" class="delete-icon">
-                        <DeleteIcon />
-                    </span>
-                </div>
-            </div>
+  <div class="associated">
+    <div class="associated-list" v-if="!disabled">
+      <div class="associated-label">Tilknyttede personer</div>
+      <div class="associated-persons-list">
+        <div v-for="(person, index) in associatedPeople" :key="index">
+          {{person}}
+          <span @click="removePerson(person)" class="delete-icon">
+            <DeleteIcon />
+          </span>
         </div>
-        <div class="add-person" v-if="!disabled">
-            <div class="associated-label">Tilknyt person</div>
-            <SelectionField @change="addPerson" :items="people" iconName="search" />
-        </div>
-        <div class="associated-list-readonly" v-if="disabled">
-            <div class="associated-label">Tilknyttede personer:</div>
-            {{ this.associatedPersonsDisabled }}
-        </div>
+      </div>
     </div>
+    <div class="add-person" v-if="!disabled">
+      <div class="associated-label">Tilknyt person</div>
+      <SelectionField placeholder="Skriv navn" @change="addPerson" :items="people" iconName="search" />
+    </div>
+    <div class="associated-list-readonly" v-if="disabled">
+      <div class="associated-label">Tilknyttede personer:</div>
+      {{ this.associatedPersonsDisabled }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -36,8 +36,10 @@ import { generalInformationActionTypes } from '@/store/modules/details/general-i
   }
 })
 export default class AssociatedPersonsInput extends Vue {
-  @Action(generalInformationActionTypes.ADD_ASSOCIATED_PERSON) addAssociatedPerson: any;
-  @Action(generalInformationActionTypes.REMOVE_ASSOCIATED_PERSON) removeAssociatedPerson: any;
+  @Action(generalInformationActionTypes.ADD_ASSOCIATED_PERSON)
+  addAssociatedPerson: any;
+  @Action(generalInformationActionTypes.REMOVE_ASSOCIATED_PERSON)
+  removeAssociatedPerson: any;
 
   @Prop() disabled!: boolean;
 
@@ -60,7 +62,9 @@ export default class AssociatedPersonsInput extends Vue {
   }
 
   get associatedPersonsDisabled() {
-    return this.$store.state.details.generalInformation.associatedPersons.join(', ');
+    return this.$store.state.details.generalInformation.associatedPersons.join(
+      ', '
+    );
   }
 }
 </script>
@@ -70,7 +74,7 @@ export default class AssociatedPersonsInput extends Vue {
 
 .associated {
   display: flex;
-  padding: $size-unit*3 $size-unit $size-unit*2 $size-unit; 
+  padding: $size-unit * 3 0 $size-unit * 2 0;
   .associated-list {
     width: 60%;
     .associated-persons-list {
@@ -95,10 +99,11 @@ export default class AssociatedPersonsInput extends Vue {
     }
   }
   .add-person {
+    margin-left: $size-unit;
+
     .input-group {
       padding: 0;
     }
-    margin-left: 100px;
   }
 
   /deep/ .input-group--autocomplete {
@@ -106,5 +111,10 @@ export default class AssociatedPersonsInput extends Vue {
       transform: none !important;
     }
   }
+}
+
+.associated-label {
+  @include field-label;
+  padding-bottom: $size-unit/4;
 }
 </style>
