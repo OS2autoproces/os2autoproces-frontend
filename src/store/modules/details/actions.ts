b9c02907538bba0cda9ee9fa59316d567758a2ec
christianbondesen <cbb@it-minds.dk>
2018-06-21 16:21:00 +0200

    store setup for details, interval disabled and value

--- src/store/modules/details/actions.ts
+++ src/store/modules/details/actions.ts
@@ -2,15 +2,20 @@ import { ActionTree } from 'vuex';
 import { DetailsState } from '@/store/modules/details/state';
 import { RootState } from '@/store/store';
 import router from '@/router';
+import { detailsMutationTypes } from '@/store/modules/details/mutations';
 
 export const namespace = 'details';
 
 export const detailsActionTypes = {
   SAVE: `${namespace}/save`,
-  REPORT: `${namespace}/report`
+  REPORT: `${namespace}/report`,
+  UPDATE: `${namespace}/update`
 };
 
 export const actions: ActionTree<DetailsState, RootState> = {
+  update({commit}, payload: Partial<DetailsState>) {
+    commit(detailsMutationTypes.UPDATE, payload);
+  },
   save() {
     // TODO: Save to backend
     // const process = await backend.put(store.proces);
