c09f50fa5c67109a232eb0331c025d3ab58a1c32
christianbondesen <cbb@it-minds.dk>
2018-06-21 11:25:46 +0200

    selection field text color

--- src/components/common/inputs/SelectionField.vue
+++ src/components/common/inputs/SelectionField.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-select v-if="!disabled" :items="items" single-line @change="valueChanged" autocomplete :value="value" :append-icon="iconName" :placeholder="placeholder"/>
+    <v-select v-if="!disabled" :items="items" single-line @change="valueChanged" autocomplete :value="value" :append-icon="iconName" :placeholder="placeholder" />
     <div v-if="disabled">{{value}}</div>
   </div>
 </template>
@@ -29,11 +29,19 @@ export default class SelectionField extends Vue {
 @import '@/styles/variables.scss';
 .input-group--autocomplete {
   padding-top: 0 !important;
-  
+
   .input-group__input {
     border: 1px solid $color-primary;
     border-radius: 20px;
     padding-left: 13px;
+
+    .input-group--select__autocomplete {
+      color: $color-text !important;
+    }
+
+    .input-group__selections__comma {
+      color: $color-text !important;
+    }
 
     .icon {
       color: $color-primary !important;
@@ -47,5 +55,9 @@ export default class SelectionField extends Vue {
 
 .menu__content--autocomplete.menuable__content__active {
   transform: translateY(24px);
+}
+
+.list__tile__title {
+  color: $color-text !important;
 }
 </style>
