f401424c23e822be2394545d0839a70c032c622e
Rasmus Sundgaard Reimer <rsr@it-minds.dk>
2018-06-22 13:39:00 +0200

    Fix typo

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -3,5 +3,5 @@ echo "OS2_DISCOVERY = ${API_DISCOVERY}"
 echo "OS2_AUTOPROCES = ${API_AUTOPROCES}"
 
 # Write the environment variables to a json file.
-# The file is used by the frontend to read the evironment configuration.
+# The file is used by the frontend to read the environment configuration.
 envsubst < /environment-config.json > /temp.json && mv /temp.json /usr/share/nginx/html/environment-config.json && nginx -g 'daemon off;'
