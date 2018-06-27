8e04ba3adf97c866a5f6527ea8d995fb5050f9cb
Rasmus Sundgaard Reimer <rsr@it-minds.dk>
2018-06-27 10:03:45 +0200

    Use HTTP service

--- src/store/modules/auth/actions.ts
+++ src/store/modules/auth/actions.ts
@@ -2,6 +2,7 @@ import { ActionTree } from 'vuex';
 import { AuthState, UserRole } from './state';
 import { RootState } from '@/store/store';
 import { authMutationTypes } from './mutations';
+import { HTTP } from '@/services/http-service';
 
 interface WhoAmIResponse {
   uuid: string | null;
@@ -20,15 +21,12 @@ export const authActionTypes = {
 
 export const actions: ActionTree<AuthState, RootState> = {
   async loadUser({ commit }): Promise<void> {
-    // TODO: Use HTTP service
-    const response = await fetch('https://dev.os2autoproces.eu/public/whoami', { credentials: 'include' });
-    const user: WhoAmIResponse = await response.json();
+    const user = (await HTTP.get<WhoAmIResponse>('/public/whoami')).data;
 
     commit(authMutationTypes.SET_USER, user.uuid !== null ? user : null);
   },
   async logout({ commit }) {
-    // TODO: Use HTTP service
-    await fetch('https://dev.os2autoproces.eu/saml/logout', { credentials: 'include', mode: 'no-cors' });
+    await HTTP.get<void>('/saml/logout');
 
     commit(authMutationTypes.SET_USER, null);
   }
