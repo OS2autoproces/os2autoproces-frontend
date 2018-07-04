b4aa2c11743a276cf5b6778a5346cb32caf7ec4c
christianbondesen <cbb@it-minds.dk>
2018-07-04 15:01:48 +0200

    removed unused loading of processes

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,6 @@ export default class App extends Vue {
   @Action(authActionTypes.LOAD_USER) loadUser: any;
 
   mounted() {
-    // this.$store.dispatch(processActionTypes.LOAD_PROCESS_DETAILS, 770);
     this.loadUser();
   }
 }
