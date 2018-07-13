dfd17e0b0b2cae57e3c18f81cd43d5bea98390a8
christianbondesen <cbb@it-minds.dk>
2018-07-13 11:19:25 +0200

    removed redundant hooks

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,10 @@ import { commonActionTypes, Cms } from '@/store/modules/common/actions';
 @Component
 export default class App extends Vue {
   @Action(authActionTypes.LOAD_USER) loadUser: any;
-
-  beforeCreate() {}
-  created() {
+  
+  mounted() {
     this.loadUser();
   }
-  mounted() {}
 }
 </script>
 
