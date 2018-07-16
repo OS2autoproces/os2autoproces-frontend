c638606ac134bac01df9675fe0182634154ff73e
christianbondesen <cbb@it-minds.dk>
2018-07-16 15:19:01 +0200

    Municipality logo component based on cvr

---
+++ src/components/common/inputs/MunicipalityLogo.vue
@@ -0,0 +1,21 @@
+<template>
+    <img :src="src" @error="src = '/logos/placeholder.png'" alt="Kommune logo" class="image">
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class MunicipalityLogoComponent extends Vue {
+  @Prop()
+  src!: string;
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/variables.scss';
+    .image {
+        height: 85px;
+        margin: $size-unit/2;
+    }
+</style>
