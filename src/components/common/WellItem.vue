7d1c41952f3a7f8eeaa84645afc3009787fb37e5
Christian Bondesen <christianbondesen@DESKTOP-2R69NOF.localdomain>
2018-07-17 14:45:35 +0200

    Well item displays red when invalid

--- src/components/common/WellItem.vue
+++ src/components/common/WellItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="well-item-wrap">
+  <div class="well-item-wrap" :class="{invalid}">
     <div class="well-item-label" :style="{flexBasis: labelWidth}">{{label}}</div>
     <div class="well-input">
       <slot/>
@@ -8,22 +8,30 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class WellItem extends Vue {
   @Prop() label!: string;
-  @Prop({ default: '50%' })
+  @Prop({ default: "50%" })
   labelWidth!: string;
+  @Prop() invalid!: boolean;
 }
 </script>
 
 <style lang="scss">
-@import '@/styles/variables.scss';
+@import "@/styles/variables.scss";
 
 .well-item-wrap {
   display: flex;
   align-items: center;
+
+   &.invalid {
+    > .well-item-label {
+     color: red !important;
+     font-style: italic;
+    }
+  }
 
   .well-item-label {
     @include field-label;
@@ -38,4 +46,5 @@ export default class WellItem extends Vue {
     min-width: 1px;
   }
 }
+
 </style>
