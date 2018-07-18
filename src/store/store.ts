dfe00cedc753c9917ee838449a0eb2b195af5bdd
Christian Bondesen <christianbondesen@DESKTOP-2R69NOF.localdomain>
2018-07-18 15:25:00 +0200

    store types

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,11 +4,22 @@ import { process } from '@/store/modules/process';
 import { search } from '@/store/modules/search';
 import { auth } from '@/store/modules/auth';
 import { common } from '@/store/modules/common';
+import { error } from '@/store/modules/error';
+import { AuthState } from '@/store/modules/auth/state';
+import { ProcessState } from '@/store/modules/process/state';
+import { SearchState } from '@/store/modules/search/state';
+import { CommonState } from '@/store/modules/common/state';
+import { ErrorState } from '@/store/modules/error/state';
 
 Vue.use(Vuex);
 
 export interface RootState {
   version: string;
+  auth: AuthState;
+  process: ProcessState;
+  search: SearchState;
+  common: CommonState;
+  error: ErrorState;
 }
 
 const store: StoreOptions<RootState> = {
@@ -16,7 +27,8 @@ const store: StoreOptions<RootState> = {
     auth,
     process,
     search,
-    common
+    common,
+    error
   }
 };
 
