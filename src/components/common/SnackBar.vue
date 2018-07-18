46b5fce2d68cb2962be8572bf57c2321d6b0b9d8
Christian Bondesen <christianbondesen@DESKTOP-2R69NOF.localdomain>
2018-07-18 15:22:30 +0200

    SnackBar component to display errors for validation

---
+++ src/components/common/SnackBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-snackbar
+      :value="value"
+      :color="color"
+      multi-line
+      :timeout="timeout"
+      top
+    >
+    <slot />
+      <v-btn
+        dark
+        flat
+        @click="clicked"
+      >
+        OK
+      </v-btn>
+    </v-snackbar>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class SnackBarComponent extends Vue {
+  @Prop() text!: string;
+  @Prop() timeout!: number;
+  @Prop() color!: string;
+  @Prop() value!: boolean;
+
+  clicked(value: any) {
+    this.$emit("clicked", value);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.v-snack /deep/ {
+  .v-snack__content {
+    height: auto;
+    width: auto;
+  }
+}
+</style>
