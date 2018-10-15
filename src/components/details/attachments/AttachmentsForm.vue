523c84cb22defb423f49fce7026cfa004f929c5e
Rasmus Sundgaard Reimer <rsr@it-minds.dk>
2018-10-15 15:03:58 +0200

    changes from review

--- src/components/details/attachments/AttachmentsForm.vue
+++ src/components/details/attachments/AttachmentsForm.vue
@@ -1,9 +1,9 @@
 <template>
-  <FormSection v-if="!(showPlaceholder && minPhase(PhaseKeys.PREANALYSIS))" heading="Bilag" id="attachments" :disabled="state.disabled.attachmentsEdit" @edit="update({ disabled: {attachmentsEdit: $event} })">
+  <FormSection v-if="!showPlaceholder" heading="Bilag" id="attachments" :disabled="state.disabled.attachmentsEdit" @edit="update({ disabled: {attachmentsEdit: $event} })">
     <AttachmentUpload :disabled="state.disabled.attachmentsEdit" />
   </FormSection>
-  <FormSection v-else heading="Bilag" id="attachments-placeholder" disable placeholder="- bilag er først tilgængelig efter de har gemt første gang." />
-</template> 
+  <FormSection v-else heading="Bilag" id="attachments" disabled placeholder="Bilag er først tilgængelig efter de har gemt første gang." />
+</template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
@@ -11,8 +11,7 @@ import { Action, Getter } from 'vuex-class';
 import AttachmentUpload from '@/components/common/inputs/AttachmentUpload.vue';
 import FormSection from '@/components/details/FormSection.vue';
 import { processActionTypes } from '@/store/modules/process/actions';
-import { processGetterTypes } from '@/store/modules/process/getters';
-import { Phase, PhaseKeys } from '@/models/phase';
+import { Phase } from '@/models/phase';
 
 @Component({
   components: {
@@ -26,10 +25,6 @@ export default class AttachmentsForm extends Vue {
 
   @Action(processActionTypes.UPDATE)
   update: any;
-  @Getter(processGetterTypes.MIN_PHASE)
-  minPhase!: (phase: Phase) => boolean;
-
-  PhaseKeys = PhaseKeys;
 
   get state() {
     return this.$store.state.process;
