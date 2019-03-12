f8c9df1febe9a4d3b14f17e9e0eab84cbe710814
Rasmus Reimer <rsr@it-minds.dk>
2019-03-12 13:21:34 +0100

    add Discovery page

---
+++ src/views/Discovery.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { HTTP } from "@/services/http-service";
+
+@Component({
+  components: {}
+})
+export default class Discovery extends Vue {
+  async mounted() {
+    await HTTP.get("public/identityproviders");
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+</style>
