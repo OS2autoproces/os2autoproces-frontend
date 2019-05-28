020599197d77b714f7ffdc18356249893ddd8aa2
Unknown <lnf@it-minds.dk>
2019-05-28 11:35:55 +0200

    changed clear action to reset

--- src/components/search/SearchFiltersActions.vue
+++ src/components/search/SearchFiltersActions.vue
@@ -34,22 +34,43 @@
 
 <template>
   <transition name="slide-y-reverse-transition">
-    <div class="actions" v-if="filtersTouched">
-      <Button class="action elevation-3" v-ripple @click="dispatchClearFilters()">Ryd filtre</Button>
+    <div
+      class="actions"
+      v-if="filtersTouched"
+    >
+      <Button
+        class="action elevation-3"
+        v-ripple
+        @click="dispatchClearFilters()"
+      >Ryd filtre</Button>
       <Button
         class="action elevation-3"
         v-ripple
         @click.stop.prevent="toggleSaveFiltersDialog()"
       >Gem Søgning</Button>
-      <AppDialog :open="saveFiltersDialogOpen" @close="closeSaveFilterDialog">
+      <AppDialog
+        :open="saveFiltersDialogOpen"
+        @close="closeSaveFilterDialog"
+      >
         <DialogContent>
           <h2 class="form-header">Gem din søgning</h2>
           <div class="save-filters-form">
-            <InputField class="input" placeholder="Navn" :value="name" @change="onNameChange"></InputField>
-            <span class="error--text" v-if="!!error">{{error}}</span>
+            <InputField
+              class="input"
+              placeholder="Navn"
+              :value="name"
+              @change="onNameChange"
+            ></InputField>
+            <span
+              class="error--text"
+              v-if="!!error"
+            >{{error}}</span>
             <div class="dialog-actions">
               <Button @click="closeSaveFilterDialog()">Annullér</Button>
-              <Button :primary="true" @click="submitSaveFilters()">GEM</Button>
+              <Button
+                :primary="true"
+                @click="submitSaveFilters()"
+              >GEM</Button>
             </div>
           </div>
         </DialogContent>
@@ -78,7 +99,7 @@ import { SavedSearchFilters } from '../../store/modules/search/state';
   }
 })
 export default class SearchFiltersActions extends Vue {
-  @Action(searchActionTypes.CLEAR_FILTERS) dispatchClearFilters!: VoidFunction;
+  @Action(searchActionTypes.RESET_FILTERS) dispatchClearFilters!: VoidFunction;
   @Action(searchActionTypes.SAVE_FILTERS) dispatchSaveFilters!: (name: string) => void;
 
   @State((state: RootState) => state.search.savedFilters) savedFilters!: SavedSearchFilters[];
