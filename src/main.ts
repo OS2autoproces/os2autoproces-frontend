39e133d5dbd16ae62e780425b01f6066a892b659
Troels Thorsen <trauls91@gmail.com>
2020-05-21 11:34:36 +0200

    New babel imports. Added vue.config.js with transpileDeps to the new masking lib and vuetify.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import 'babel-polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
